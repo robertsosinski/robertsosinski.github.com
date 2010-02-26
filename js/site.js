fbb1571c4c5bd938a908117e133935c6f317d93d
Robert Sosinski <email@robertsosinski.com>
2010-02-26 14:11:17 -0500

    more design work

--- js/site.js
+++ js/site.js
@@ -1,7 +1,31 @@
 (function($) {
   $(function() {
-    $.getJSON("http://github.com/api/v2/json/repos/show/robertsosinski?callback=?", function(json) {
-        new EJS({url:'/js/projects.ejs'}).update('projects_slot', json);
+    (function() {
+      // fetch and insert projects from github json api
+      if ($("#projects").length == 1) {
+        $.getJSON("http://github.com/api/v2/json/repos/show/robertsosinski?callback=?", function(json) {
+          new EJS({url: "/js/projects.ejs"}).update("projects", json);
+        });
+      }
+      
+      // obfuscate email address
+      var a = "mail";
+      var b = "to:"
+      var c = "email";
+      var d = document.location.hostname.replace("github.", "");
+      var e = (a+b+c+"@"+d);
+            
+      $("a.email").attr("href", e);
+    })();
+    
+    // set category event handlers
+    $(".category").each(function() {
+      var $this = $(this);
+
+      $this.click(function() {
+        $("#categories h3, #categories a").css({"font-weight": null});
+        $("#categories " + $this.attr("href") + " h3, #categories " + $this.attr("href") + " a").css({"font-weight": "bolder"});
+      });
     });
-  });
+  });  
 })(jQuery);
